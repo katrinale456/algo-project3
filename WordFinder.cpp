8982564fc2c8def78e1443466712ea53ffd44ffa
katrinale456 <113145732+katrinale456@users.noreply.github.com>
2023-04-13 21:55:50 -0400

    Add files via upload

---
+++ WordFinder.cpp
@@ -0,0 +1,64 @@
+#include "WordFinder.h"
+#include "sort_algorithms.h"
+
+WordFinder::WordFinder() {
+	numWords = 0;
+	words;
+}
+
+void WordFinder::readWords() {
+	ifstream fin;
+	string line;
+	string text;
+	fin.open("Glossary.txt");
+	
+	if (!fin) {
+		cerr << "Cannot open 'Glossary.txt'" << endl;
+		exit(1);
+	}
+
+	while (getline(fin, line)) {
+		fin >> text;
+		numWords += 1;
+		words.push_back(text);
+	}
+	
+	fin.close();
+}
+
+void WordFinder::sortWords() {
+	//cout << "start";
+	selectionSort(words);
+	//cout << "end";
+}	
+
+int WordFinder::lookupWords(string target) {
+	int first = 0;
+	int last = numWords-1;
+	int mid = 0;
+
+	while (first <= last) {
+		mid = (first + last)/2;
+
+		if (target < words.at(mid)) {
+			last = mid - 1;
+		}
+		else if (target > words.at(mid)) {
+			first = mid + 1;
+		}	
+		else {
+			return (mid);
+		}	
+	}
+	if (first > last) {
+		return -1;
+	}
+}
+
+ostream& operator<< (ostream& ostr, const WordFinder &x) {
+	for (int i = 0; i < x.numWords; i++) {
+		ostr << x.words.at(i) << endl;
+	}
+	return ostr;
+}
+
